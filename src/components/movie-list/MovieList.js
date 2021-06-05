06fce0eff2b4ec48438b00290d41a920b448a3f0
magusin <fanatsy4@gmail.com>
2021-06-05 19:49:29 +0200

    refactorisation MovieList

--- src/components/movie-list/MovieList.js
+++ src/components/movie-list/MovieList.js
@@ -3,14 +3,17 @@ import MovieElement from './MovieElement';
 
 export default class MovieList extends Component {
 
+
   render() {
     return (
       <div className="w-75 d-flex flex-row flex-wrap align-content-start">
-  <MovieElement movie={ this.props.movie[0] } updateSelectedMovie={this.props.updateSelectedMovie}/>
-  <MovieElement movie={ this.props.movie[1] } updateSelectedMovie={this.props.updateSelectedMovie}/>
-  <MovieElement movie={ this.props.movie[2] } updateSelectedMovie={this.props.updateSelectedMovie}/>
-  <MovieElement movie={ this.props.movie[3] } updateSelectedMovie={this.props.updateSelectedMovie}/>
+        {this.props.movies.map((m, index) => (
+          <MovieElement key={m.title + index}
+            movie={m}
+            updateSelectedMovie={() => { this.props.updateSelectedMovie(index) }} />
+        ))}
       </div>
     );
   }
+
 }
